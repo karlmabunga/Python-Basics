ad518116641b657643e752e84ddcf3bcc707c445
Karl Mabunga <karlmabunga@yahoo.com>
2024-04-29 23:07:54 -0700

    Create operatores file and add explanation for arithmetic operators

---
+++ operators.py
@@ -0,0 +1,31 @@
+# Operators in python
+
+# Types
+# 1 arithmetic operators
+# 2 assignment operators
+# 3 comparison operators
+# 4 identity operators
+# 5 membership operators
+# 6 bitwise operators
+
+# 1 Arithmetic operators include:
+# + Adds numbers
+# - Subtracts numbers
+# * Multiplies numbers
+# / Divides numbers
+# % - See below for explanation
+# ** - See below for explanation
+# // - See below for explanation
+
+# % or modulo is will leave you with remainder
+
+print(9 % 2)
+# Prints 1 because after dividing 9 by 2 as many times as it can, there is a remainder of 1
+
+# // or floor division will divide and round down
+print(9 // 2)
+# Prints 4 even tho answer is 4.5
+
+# ** Exponentiation will have the number multiplied by itself N times
+print(10 ** 3)
+# Prints 1000 since 10 * 10 * 10 is 1000
