f4a67bb25724fbb14a28a5cd983b1d6e6175f967
Karl Mabunga <karlmabunga@yahoo.com>
2024-05-29 13:43:42 -0700

    Solve for Fibonacci and Spiral Order

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -109,12 +109,66 @@ def generateMatrix(n):
     return res
 
 
-print(generateMatrix(3))
+# print(generateMatrix(3))
 #[[1,2,3],
 # [8,9,4],
 # [7,6,5]]
-print(generateMatrix(4))
+# print(generateMatrix(4))
 #  [[1, 2, 3, 4], 
 # [12, 13, 14, 5], 
 # [11, 16, 15, 6], 
 #  [10, 9, 8, 7]]
+
+def spiralOrder(matrix):
+    res = []
+    left, right = 0, len(matrix[0]) - 1
+    top, bottom = 0, len(matrix) - 1
+
+    while left <= right and top <= bottom:
+        for col in range(left, right + 1):
+            res.append(matrix[top][col])
+        top += 1
+
+        for row in range(top, bottom + 1):
+            res.append(matrix[row][right])
+        right -= 1
+
+        # if left > right or bottom > top:
+        #     break
+
+        for col in range(right, left - 1, -1):
+            res.append(matrix[bottom][col])
+        bottom -= 1
+
+        for row in range(bottom, top - 1, -1):
+            res.append(matrix[row][left])
+        left += 1
+
+    return res
+
+# print(spiralOrder([[1,2,3],
+#                    [4,5,6],
+#                    [7,8,9]]))
+# [1,2,3,6,9,8,7,4,5]
+# print(spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+# [1,2,3,4,8,12,11,10,9,5,6,7]
+
+
+def fib(n, memo = {}):
+    if n < 2:
+        return n
+    if n in memo:
+        return memo[n]
+    
+    res = fib(n-1, memo) + fib(n - 2, memo)
+    memo[n] = res
+    return res
+
+def slowFib(n):
+    if n < 2:
+        return n
+    return slowFib(n - 1) + slowFib(n - 2)
+
+# [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+# print(fib(365))
+# print(slowFib(35))
