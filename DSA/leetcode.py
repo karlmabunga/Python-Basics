746ef94d84141c6f41be5e14373611120ef3eed5
Karl Mabunga <karlmabunga@yahoo.com>
2024-06-10 22:53:45 -0700

    Solve algorithim for Permutation String

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -437,5 +437,54 @@ def characterReplacement(s, k):
 # Output: 4
 # Input: s = "AAABABB", k = 1
 # Output: 5
-print(characterReplacement('XXYK', 2))
-print(characterReplacement('AAABABB', 1))
+# print(characterReplacement('XXYK', 2))
+# print(characterReplacement('AAABABB', 1))
+
+################################################################################################################################################################
+
+# Permutation String
+# You are given two strings s1 and s2.
+# Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+# Both strings only contain lowercase letters.
+
+def checkInclusion(s1, s2):
+    if len(s1) > len(s2):
+        return False
+    s1Count, s2Count = [0] * 26, [0] * 26
+    for i in range(len(s1)):
+        s1Count[ord(s1[i]) - ord('a')] += 1
+        s2Count[ord(s2[i]) - ord('a')] += 1
+
+    matches = 0
+    for i in range(len(s1Count)):
+        matches += (1 if s1Count[i] == s2Count[i] else 0)
+    
+    l = 0
+    for r in range(len(s1), len(s2)):
+        if matches == 26: return True
+
+        i = ord(s2[r]) - ord('a')
+        s2Count[i] += 1
+        if s2Count[i] == s1Count[i]:
+            matches += 1
+        elif s1Count[i] + 1 == s2Count[i]:
+            matches -= 1
+        
+        i = ord(s2[l]) - ord('a')
+        s2Count[i] -= 1
+        if s2Count[i] == s1Count[i]:
+            matches += 1
+        elif s1Count[i] - 1 == s2Count[i]:
+            matches -= 1
+
+        l += 1
+
+    return matches == 26
+            
+# Input: s1 = "abc", s2 = "lecabee"
+# Output: true
+# Input: s1 = "abc", s2 = "lecaabee"
+# Output: false
+# print(checkInclusion('abc', 'lecabee'))
+# print(checkInclusion('abc', 'lecaabee'))
+# print(checkInclusion('adc', 'dcda'))
