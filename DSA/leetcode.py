4ef4dac95d8e9be6424be06d79b3afc4d4b50c80
Karl Mabunga <karlmabunga@yahoo.com>
2024-06-21 19:01:27 -0700

    Solve algo for Validate parentheses (Stack Problem)

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -621,4 +621,42 @@ def countVowelSubstrings(word):
 # Explanation: The vowel substrings of word are as follows:
 # - "aeiou"
 # - "aeiouu"
-print(countVowelSubstrings('aeiouu'))
+# print(countVowelSubstrings('aeiouu'))
+
+################################################################################################################################################################
+
+# Validate Parentheses
+
+# You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+# The input string s is valid if and only if:
+# Every open bracket is closed by the same type of close bracket.
+# Open brackets are closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+# Return true if s is a valid string, and false otherwise.
+
+def isValid(s):
+    pairs = {
+        '}': '{',
+        ']': '[',
+        ')': '('
+    }
+    stack = []
+    for c in s:
+        if c in pairs:
+            if stack and pairs[c] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+    return True if not stack else False
+
+# Input: s = "[]"
+# Output: true
+# Input: s = "([{}])"
+# Output: true
+# Input: s = "[(])"
+# Output: false
+print(isValid('[]'))
+print(isValid("([{}])"))
+print(isValid('[(])'))
