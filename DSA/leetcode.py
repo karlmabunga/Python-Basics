1ba57cfc54a299c4b37c0a1684912f3dd5933889
Karl Mabunga <karlmabunga@yahoo.com>
2024-06-26 23:04:32 -0700

    Solve class MinStack leetcode

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -657,6 +657,40 @@ def isValid(s):
 # Output: true
 # Input: s = "[(])"
 # Output: false
-print(isValid('[]'))
-print(isValid("([{}])"))
-print(isValid('[(])'))
+# print(isValid('[]'))
+# print(isValid("([{}])"))
+# print(isValid('[(])'))
+
+
+# Minimum Stack
+# Design a stack class that supports the push, pop, top, and getMin operations.
+
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# Each function should run in O(1) time.
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.stack[-1] if len(self.stack) else val)
+        self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
