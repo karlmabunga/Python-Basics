1f0013a4470b5c160df5db9eae89fcbd56f752fc
Karl Mabunga <karlmabunga@yahoo.com>
2024-07-16 17:42:17 -0700

    Create TimeMap class and implement getter and setter function

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -1,5 +1,6 @@
 import collections
 import math
+import time
 
 
 def stringReversal(s):
@@ -1130,6 +1131,67 @@ def search(nums, target):
 # Output: -1
 # Input: nums=[1,3], target=3
 # Output: 1
-print(search([3,4,5,6,1,2], 1))
-print(search([3,5,6,0,1,2], 4))
-print(search([1,3], 3))
+# print(search([3,4,5,6,1,2], 1))
+# print(search([3,5,6,0,1,2], 4))
+# print(search([1,3], 3))
+
+
+################################################################################################################################################################
+
+
+# Time Based Key-Value Store
+# Implement a time-based key-value data structure that supports:
+
+# Storing multiple values for the same key at specified time stamps
+# Retrieving the key's value at a specified timestamp
+# Implement the TimeMap class:
+
+# TimeMap() Initializes the object.
+
+# void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+
+# String get(String key, int timestamp) Returns the most recent value of key if set was previously called on it 
+# and the most recent timestamp for that key prev_timestamp is less than or equal to the given timestamp (prev_timestamp <= timestamp). 
+
+# If there are no values, it returns "".
+# Note: For all calls to set, the timestamps are in strictly increasing order.
+
+class TimeMap:
+
+    def __init__(self):
+        self.data = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.data:
+            self.data[key] = []
+        self.data[key].append([value, timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ''
+        values = self.data.get(key, [])
+
+        l, r = 0, len(values) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+    
+timemap = TimeMap()
+
+timemap.set('KM', 'Fantasy', 1)
+timemap.set('KM', 'Football', 2)
+timemap.set('RM', 'Fantasy', 3)
+print(timemap.get('KM', 1))
+# Expect: Fantasy
+print(timemap.get('KM', 2))
+# Expect: Football
+print(timemap.get('RM', 5))
+# Expect: Fantasy
+print(timemap.get('KM', 12))
+# Expect: Football
