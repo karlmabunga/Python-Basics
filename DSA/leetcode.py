721bc5592a9f906660864c6ad71540398663da3a
Karl Mabunga <karlmabunga@yahoo.com>
2024-07-22 22:25:04 -0700

    Solve algo for merging two sorted LinkedLists

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -1260,6 +1260,10 @@ def findMedianSortedArrays(nums1, nums2):
 
 ################################################################################################################################################################
 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 # Reverse a Linked List
 # Solved 
@@ -1291,3 +1295,50 @@ def reverseList(head):
 
 # 0 <= The length of the list <= 1000.
 # -1000 <= Node.val <= 1000
+
+################################################################################################################################################################
+
+# Merge Two Sorted Lists
+# Solved
+# Easy
+# Topics
+# Companies
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+ 
+
+# Example 1:
+
+
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# Example 2:
+
+# Input: list1 = [], list2 = []
+# Output: []
+# Example 3:
+
+# Input: list1 = [], list2 = [0]
+# Output: [0]
+
+def mergeTwoLists(list1, list2):
+    start = curr = ListNode()
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            curr.next = list1
+            list1 = list1.next
+        else:
+            curr.next = list2
+            list2 = list2.next
+        curr = curr.next
+    curr.next = list1 or list2
+    return start.next
+
+
+################################################################################################################################################################
+
