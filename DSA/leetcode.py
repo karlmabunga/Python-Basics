00071b5dd64ca7f140c4a5c161aa69e3af5d82b4
Karl Mabunga <karlmabunga@yahoo.com>
2024-07-26 22:31:16 -0700

    Solve algorithm for finding duplicate using Floyd's algorithm

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -1561,8 +1561,22 @@ def hasCycle(head):
 # 1 <= nums[i] <= n
 
 def findDuplicate(nums):
-    pass
+    slow, fast = 0, 0 
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    slow2 = 0
+    while True:
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if slow == slow2:
+            return slow
+
 
 
 print(findDuplicate([1,2,3,2,2]))
 print(findDuplicate([1,2,3,4,4]))
+
+################################################################################################################################################################
