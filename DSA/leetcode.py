7560d8869b44631ccca72e03b89fecdeb39d20ec
Karl Mabunga <karlmabunga@yahoo.com>
2024-08-06 16:12:57 -0700

    Solve algo for least recently used cache.

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -1580,3 +1580,81 @@ print(findDuplicate([1,2,3,2,2]))
 print(findDuplicate([1,2,3,4,4]))
 
 ################################################################################################################################################################
+
+# LRU Cache
+# Implement the Least Recently Used (LRU) cache class LRUCache. The class should support the following operations
+
+# LRUCache(int capacity) Initialize the LRU cache of size capacity.
+# int get(int key) Return the value cooresponding to the key if the key exists, otherwise return -1.
+# void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the introduction of the new pair causes the cache to exceed its capacity, remove the least recently used key.
+# A key is considered used if a get or a put operation is called on it.
+
+# Ensure that get and put each run in O(1) average time complexity.
+
+# Example 1:
+
+# Input:
+# ["LRUCache", [2], "put", [1, 10],  "get", [1], "put", [2, 20], "put", [3, 30], "get", [2], "get", [1]]
+
+# Output:
+# [null, null, 10, null, null, 20, -1]
+
+# Explanation:
+# LRUCache lRUCache = new LRUCache(2);
+# lRUCache.put(1, 10);  // cache: {1=10}
+# lRUCache.get(1);      // return 10
+# lRUCache.put(2, 20);  // cache: {1=10, 2=20}
+# lRUCache.put(3, 30);  // cache: {2=20, 3=30}, key=1 was evicted
+# lRUCache.get(2);      // returns 20 
+# lRUCache.get(1);      // return -1 (not found)
+# Constraints:
+
+# 1 <= capacity <= 100
+# 0 <= key <= 1000
+# 0 <= value <= 1000
+
+
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.next = self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cap, self.cache = capacity, {}
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # remove node
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    # insert at right
+    def insert(self, node):
+        prev, next = self.right.prev, self.right
+        prev.next = next.prev = node
+        node.prev, node.next = prev, next
+
+    def get(self, key):
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+
+################################################################################################################################################################
