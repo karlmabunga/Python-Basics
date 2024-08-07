e87038f8a5c3dfd387d29898e536ea7d3d121034
Karl Mabunga <karlmabunga@yahoo.com>
2024-08-07 14:40:28 -0700

    Solve algorithm for merge K sorted Linked Lists

--- DSA/leetcode.py
+++ DSA/leetcode.py
@@ -1658,3 +1658,58 @@ class LRUCache:
 
 
 ################################################################################################################################################################
+
+# Merge K Sorted Linked Lists
+# You are given an array of k linked lists lists, where each list is sorted in ascending order.
+
+# Return the sorted linked list that is the result of merging all of the individual linked lists.
+
+# Example 1:
+
+# Input: lists = [[1,2,4],[1,3,5],[3,6]]
+
+# Output: [1,1,2,3,3,4,5,6]
+# Example 2:
+
+# Input: lists = []
+
+# Output: []
+# Example 3:
+
+# Input: lists = [[]]
+
+# Output: []
+# Constraints:
+
+# 0 <= lists.length <= 1000
+# 0 <= lists[i].length <= 100
+# -1000 <= lists[i][j] <= 1000
+
+def mergeKLists(lists):
+    if not lists or len(lists) == 0:
+        return None
+    while len(lists) > 1:
+        merged = []
+        for i in range(0, len(lists), 2):
+            l1 = lists[i]
+            l2 = lists[i + 1] if (i + 1) < len(lists) else None
+            merged.append(mergeLists(l1,l2))
+        lists = merged
+    return lists[0]
+
+def mergeLists(l1,l2):
+    dummy = tail = ListNode()
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+    if l1:
+        tail.next = l1
+    if l2:
+        tail.next = l2
+    return dummy.next
+        
